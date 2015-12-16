73030d6a8ee353b9b329ecd00a05603efb709eef
chrismo <chrismo@clabs.org>
2015-12-16 10:20:17 -0600

    Sketch for initial battle screen.
    Added in a bunch of stuff from jlongster/canvas-game-bootstrap, good
    stuff.
    
    Result right now is just placeholder player and monster characters
    bouncing.

---
+++ js/resources.js
@@ -0,0 +1,62 @@
+// https://github.com/jlongster/canvas-game-bootstrap/blob/a878158f39a91b19725f726675c752683c9e1c08/js/resources.js
+(function() {
+        var resourceCache = {};
+        var loading = [];
+        var readyCallbacks = [];
+
+        // Load an image url or an array of image urls
+        function load(urlOrArr) {
+            if(urlOrArr instanceof Array) {
+                urlOrArr.forEach(function(url) {
+                    _load(url);
+                });
+            }
+            else {
+                _load(urlOrArr);
+            }
+        }
+
+        function _load(url) {
+            if(resourceCache[url]) {
+                return resourceCache[url];
+            }
+            else {
+                var img = new Image();
+                img.onload = function() {
+                    resourceCache[url] = img;
+
+                    if(isReady()) {
+                        readyCallbacks.forEach(function(func) { func(); });
+                    }
+                };
+                resourceCache[url] = false;
+                img.src = url;
+            }
+        }
+
+        function get(url) {
+            return resourceCache[url];
+        }
+
+        function isReady() {
+            var ready = true;
+            for(var k in resourceCache) {
+                if(resourceCache.hasOwnProperty(k) &&
+                   !resourceCache[k]) {
+                    ready = false;
+                }
+            }
+            return ready;
+        }
+
+        function onReady(func) {
+            readyCallbacks.push(func);
+        }
+
+        window.resources = {
+            load: load,
+            get: get,
+            onReady: onReady,
+            isReady: isReady
+        };
+    })();
